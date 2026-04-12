4daebc69c9bdd663b8243a10d6f2d5c1e25b7c43
komal-1906 <komal1906k@gmail.com>
2026-04-13 00:46:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn")
+const newPass = document.getElementById("newPass")
+
+btn.addEventListener('click', () => {
+    newPass.innerText = "New Password Generated!!"
+})
